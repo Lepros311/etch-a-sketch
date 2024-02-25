bfe3f65a640e363703445606860cd9fcd12c49f4
Lepros311 <65778034+Lepros311@users.noreply.github.com>
2024-02-25 18:10:01 -0500

    Add event listeners to add color on mouseover, add new grid button, and add input validation loop

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-function createBoxes(numberPerRow) {
-    const cdiv = document.querySelector('.container');
+const btn = document.querySelector('.btn');
+const cdiv = document.querySelector('.container');
+let numberPerRow = null;
+
+function getNumberPerRow() {
+    while (true) {
+    numberPerRow = prompt("How many squares per side for your grid? (2-100)");
+    numberPerRow = parseInt(numberPerRow);
+    if ((numberPerRow <= 100) && (numberPerRow >= 2)) {
+        break;
+    }
+    alert("That is not a valid option. Please provide a number between 2 and 100.");
+}
+return numberPerRow;
+};
+
+function clearBoxes() {
+    cdiv.innerHTML = null;
+}
+
+function createBoxes() {
+    numberPerRow = getNumberPerRow();
+    
     const total = (numberPerRow * numberPerRow) + numberPerRow;
     const mod = numberPerRow + 1;
+    
   
-    for (let i = 1; i < total; i++) {
-      const div = document.createElement('div');
-  
+    for (let i = 1; i < total; i++) {  
+        let div = document.createElement('div');
+
       if (i % mod === 0) {
         div.style.cssText = "border: 0; height: 0; width: 100%";
       } else {
@@ -13,7 +35,19 @@ function createBoxes(numberPerRow) {
       }
   
       cdiv.appendChild(div);
-    }
-  }
-  
-  createBoxes(16);
+    };
+    const divs = cdiv.querySelectorAll('div');
+    divs.forEach(function(div) {
+        div.addEventListener('mouseover', function(e) {
+            div.classList.toggle('color');
+        });
+    });
+};
+
+btn.addEventListener('click', () => {
+    clearBoxes();
+    createBoxes();
+})
+
+createBoxes();
+
